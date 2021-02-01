ab6898ef68667efe4e45ae4c0dc02b7849d3e06b
Jacob Kohl <jkohl7@my.wctc.edu>
2021-02-01 15:05:33 -0600

    update

---
+++ js/client.js
@@ -0,0 +1,14 @@
+$(function(){
+     // preload audio
+     var toast = new Audio('media/toast.wav');
+    $('.code').on('click', function(e) {
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        e.preventDefault();
+            // play audio
+            toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
